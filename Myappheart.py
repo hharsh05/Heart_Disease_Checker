8db9a958bb2b3618246d31ee7c4c66ce3b36de71
Harshit Harsh <104749996+hharsh05@users.noreply.github.com>
2022-07-15 16:00:24 +0530

    Update Myappheart.py

--- Myappheart.py
+++ Myappheart.py
@@ -11,7 +11,7 @@ null=None
 def load_images(file_name):
     img = Image.open(file_name)
     return st.image(img,width=300)
-img='appDiabetes.JPG'
+img='heartapp.jpg'
 load_images(img)
 # Title
 st.header("Heart Disease Checker app")
